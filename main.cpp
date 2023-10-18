b86d4b30d99886608f186f705914a5b5805a498f
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2023-10-18 21:38:29 +0400

    add some more animation assets

--- main.cpp
+++ main.cpp
@@ -4,4 +4,14 @@
 
 #include <core/game.hpp>
 
-int main() {}
+int main() {
+    {
+        auto& game = sdl::Game::getInstance();
+        game.init("Bomberman", 800, 640, 0);
+        game.registerAnimatableObject("../assets/animation/anim3.png",
+                                    SDL_FRect{0, 0, 256, 256},
+                                    SDL_FRect{300, 300, 256, 256},
+                                    SDL_Point{0, 0}, SDL_Point{0, 0}, 6, 8, 0);
+        game.run();
+    }
+}
