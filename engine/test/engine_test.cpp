432882087da56318b6788f217c5726584113c02b
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2023-10-28 21:57:10 +0400

    add todo comment for 'UI' test feeature

--- engine/test/engine_test.cpp
+++ engine/test/engine_test.cpp
@@ -3,6 +3,7 @@
 #include <engine/engine.hpp>
 
 TEST(Engine, Animation) {
+    // TODO: Add future of 'UI' testing with stored textures per frame
     sdl::Engine engine("Bomberman", 800, 640, 0);
     engine.registerAnimatableSprite("../assets/animation/anim3.png",
                                     {{0, 0, 256, 256}},
