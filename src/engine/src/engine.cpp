3ad3d3fefb71f5423ef34039fc6df1b5491d47ef
VacheGaspar <vache.gasparyan3@gmail.com>
2023-12-10 15:00:01 +0400

    enable sanitizers

--- src/engine/src/engine.cpp
+++ src/engine/src/engine.cpp
@@ -52,7 +52,7 @@ void Engine::registerSprite(const std::string& assetPath,
     load(assetPath);
     auto sdlSrcRect = SDL_FRect{srcRect[0], srcRect[1], srcRect[2], srcRect[3]};
     auto sdlDstRect = SDL_FRect{dstRect[0], dstRect[1], dstRect[2], dstRect[3]};
-    _sprites[assetPath] = std::make_unique<Sprite>(std::move(Sprite(_textures[assetPath], sdlSrcRect, sdlDstRect)));
+    _sprites[assetPath] = std::make_unique<Sprite>(Sprite(_textures[assetPath], sdlSrcRect, sdlDstRect));
 }
 
 void Engine::registerAnimatableSprite(const std::string& assetPath,
@@ -63,9 +63,9 @@ void Engine::registerAnimatableSprite(const std::string& assetPath,
     load(assetPath);
     auto sdlSrcRect = SDL_FRect{srcRect[0], srcRect[1], srcRect[2], srcRect[3]};
     auto sdlDstRect = SDL_FRect{dstRect[0], dstRect[1], dstRect[2], dstRect[3]};
-    _sprites[assetPath] = std::make_unique<AnimatableSprite>(std::move(AnimatableSprite(_textures[assetPath], sdlSrcRect,
+    _sprites[assetPath] = std::make_unique<AnimatableSprite>(AnimatableSprite(_textures[assetPath], sdlSrcRect,
                                                                     sdlDstRect, spriteRowCount,
-                                                                    spriteColCount, animationSpeed)));
+                                                                    spriteColCount, animationSpeed));
 }
 
 void Engine::setDrawColor(SDL_Color color) {
