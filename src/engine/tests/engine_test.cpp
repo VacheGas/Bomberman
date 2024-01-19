d25f422f414b7e0bb01d3fff0058975ee00331ee
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2024-01-19 12:59:22 +0400

    restore the TODO comment in engine test which was mistalenly removed

--- src/engine/tests/engine_test.cpp
+++ src/engine/tests/engine_test.cpp
@@ -3,8 +3,8 @@
 #include "engine/engine.hpp"
 
 TEST(Engine, Animation) {
+    // TODO: Add future of 'UI' testing with stored textures per frame
     sdl::Engine engine("Bomberman", 800, 640, 0);
-    engine.registerGraphicElement(
-        std::string(RESOURCES_PATH "json/explosion_1.json"), {{0, 0, 256, 256}});
+    engine.registerGraphicElement(std::string(RESOURCES_PATH "json/explosion_1.json"), {{0, 0, 256, 256}});
     engine.run();
 }
