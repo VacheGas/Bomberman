8401e99b5c523ce7852f8d5856232a00a21d671d
VacheGaspar <vache.gasparyan3@gmail.com>
2024-02-05 17:04:48 +0400

    fixed window_test.cpp

--- src/engine/tests/window_test.cpp
+++ src/engine/tests/window_test.cpp
@@ -7,11 +7,11 @@
 #include <engine/window.hpp>
 
 TEST(Window, Get) {
-    std::string_view title ("test_name");
-    std::unique_ptr<sdl::Window> window = sdl::Window::createWindow(0, 300,  160, title);
+    std::string_view title("test_name");
+    sdl::Window window = sdl::Window(title, 300, 160, 0);
 
-    EXPECT_EQ(window->flags(), 0);
-    EXPECT_EQ(window->width(), 300);
-    EXPECT_EQ(window->height(), 160);
-    EXPECT_EQ(window->title(), title);
+    EXPECT_EQ(window.flags(), 0);
+    EXPECT_EQ(window.width(), 300);
+    EXPECT_EQ(window.height(), 160);
+    EXPECT_EQ(window.title(), title);
 }
